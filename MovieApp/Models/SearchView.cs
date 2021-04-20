47c3e1ecd4b9d4a3af79b6e4ed2c8384377aa51e
kawakami <mao.kawakami@frontarc.co.jp>
2021-04-20 14:18:25 +0900

    "Search"→"SearchView"に変更、検索機能追加

---
+++ MovieApp/Models/SearchView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+    public class SearchView
+    {
+        [DisplayName("ジャンル名")]
+        public string Kind { get; set; }
+
+        [DisplayName("タイトル名")]
+        public string Title { get; set; }
+
+        public List<Movie> Movies { get; set; }
+    }
+}
