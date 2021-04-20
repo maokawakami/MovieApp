0c3c5e2a81dc67b7bbc17d03bef3540a4783aaf8
kawakami <mao.kawakami@frontarc.co.jp>
2021-04-20 10:12:40 +0900

    検索機能追加

---
+++ MovieApp/Models/Search.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Models
+{
+    public class Search
+    {
+        [DisplayName("金額")]
+        [RegularExpression(@"[0-9]+")]
+        public string Price { get; set; }
+    }
+}
