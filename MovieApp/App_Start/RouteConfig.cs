d447818bf0cec097c7c5495ac4d17eef1c8e2bad
kawakami <mao.kawakami@frontarc.co.jp>
2021-04-20 17:46:39 +0900

    表示される文字の変更

--- MovieApp/App_Start/RouteConfig.cs
+++ MovieApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MovieApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Movies", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Movies", action = "Search", id = UrlParameter.Optional }
             );
         }
     }
