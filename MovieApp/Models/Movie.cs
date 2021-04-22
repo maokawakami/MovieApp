dc497891a852b82e76ea195bc994b5f4646ea14f
kawakami <mao.kawakami@frontarc.co.jp>
2021-04-22 10:17:28 +0900

    Model(Movie)に入力制限を追加、エラーメッセージが表示されるよう変更

--- MovieApp/Models/Movie.cs
+++ MovieApp/Models/Movie.cs
@@ -11,15 +11,20 @@ namespace MovieApp.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "タイトルを入力してください")]
         [DisplayName("タイトル")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "公開日を入力してください")]
         [DisplayName("公開日")]
+        [DataType(DataType.Date)]
         public DateTime Release { get; set; }
 
+        [Required(ErrorMessage = "ジャンルを選択してください")]
         [DisplayName("ジャンル")]
         public string Kind { get; set; }
 
+        [Required(ErrorMessage = "金額を入力してください")]
         [DisplayName("金額")]
         public string Price { get; set; }
     }
